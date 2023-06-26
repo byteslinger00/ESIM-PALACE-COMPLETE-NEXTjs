5246476e18f99b47f72122c85c48589c96848be0
!Athena! Goddess Of War <132208831+21st-Athena@users.noreply.github.com>
2023-06-26 15:18:18 -0700

    delete/unnecessary-signup

--- src/components/SignUp/Logo.tsx
+++ src/components/SignUp/Logo.tsx
@@ -7,7 +7,7 @@ interface props {
 export const Logo:React.FC<props> = ({type}) => {
     return (
         <div className='p-3 rounded-full bg-[#EBF6FF]'>
-            <Image src={`/assets/Sign up/Icons/SVG Icon/${type} logo.svg`} width={24} height={24} alt="apple login" />
+            <Image src={`/assets/Sign up/Icons/${type} logo.svg`} width={24} height={24} alt="apple login" />
         </div>
     )
 }
