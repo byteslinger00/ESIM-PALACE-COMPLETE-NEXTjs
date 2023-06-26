926d923b219d30d664cafdbe00ad792069ad3990
!Athena! Goddess Of War <132208831+21st-Athena@users.noreply.github.com>
2023-06-26 14:29:03 -0700

    fix/not-showing-image

--- src/components/elements/Support/SideOption.tsx
+++ src/components/elements/Support/SideOption.tsx
@@ -8,7 +8,7 @@ interface props {
 export const Option:React.FC<props> = ({src, title}) => {
     return(
         <div className="bg-white  p-[17px] w-full font-montserratbold flex flex-row gap-3 rounded-lg text-left">
-            <Image src={src} width={22} height={22} alt="option" className='max-xl:w-[18px]'/>
+            <Image src={`/assets/Support Page/Icons/SVG Icon/${src}.svg`} width={22} height={22} alt="option" className='max-xl:w-[18px]'/>
             <p className='xl:text-[16px] text-[14px]'>{title}</p>
         </div>
     )
