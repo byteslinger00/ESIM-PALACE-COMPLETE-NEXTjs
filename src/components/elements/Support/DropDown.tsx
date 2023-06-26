6f56d65cae6ef9af7334cc67c9855d98db77e9e3
!Athena! Goddess Of War <132208831+21st-Athena@users.noreply.github.com>
2023-06-26 15:06:43 -0700

    delete/unnecessary-file-support

--- src/components/elements/Support/DropDown.tsx
+++ src/components/elements/Support/DropDown.tsx
@@ -12,7 +12,7 @@ export const DropDown: React.FC<props> = ({ title, description, visible }) => {
         <div className=" p-6 xl:text-[20px] text-[14px] gap-6 bg-[#EBF6FF] rounded-[14px] w-full flex flex-col text-left">
             <div className='flex flex-row'>
                 <h6 className="font-montserratbold grow">{title}</h6>
-                <Image src="/assets/Support page/Icons/SVG Icon/Arrow down sign to navigate.svg" width={24} height={24} alt="Arrow Down" className='cursor-pointer'/>
+                <Image src="/assets/Support page/Icons/Arrow down sign to navigate.svg" width={24} height={24} alt="Arrow Down" className='cursor-pointer'/>
             </div>
             {visible ? <hr className='border-[#AAD9FF]' /> : ''}
             {visible ? <p className="font-montserrat">{description}</p> : ""}
