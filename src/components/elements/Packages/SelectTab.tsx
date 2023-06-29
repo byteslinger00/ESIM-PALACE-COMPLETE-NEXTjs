0d9b73c967088f15c7c148b3e79822d0bd432fc7
21st-Athena <132208831+21st-Athena@users.noreply.github.com>
2023-06-29 10:11:15 -0400

    fix/tabitem-pacakges

--- src/components/elements/Packages/SelectTab.tsx
+++ src/components/elements/Packages/SelectTab.tsx
@@ -1,22 +1,71 @@
-"use client"
+"use client";
 import { useEffect, useState } from "react";
 import { TabItem } from "./TabItem";
 
 interface props {
-    setRegion: React.Dispatch<React.SetStateAction<string>>;
-    selectedRegion: string;
+  setRegion: React.Dispatch<React.SetStateAction<string>>;
+  selectedRegion: string;
 }
 
 export const SelectTab: React.FC<props> = ({ setRegion, selectedRegion }) => {
-    const [index, setIndex] = useState(2);
-    return (
-        <div className="grid grid-cols-6 border-b-[1px] border-[#F2B21B70] lg:mx-[100px] font-montserrat font-semibold max-md:hidden">
-            <TabItem title="Europe" region="Europe" selected_region={selectedRegion} set_region={setRegion} />
-            <TabItem title="North America" region="North America" selected_region={selectedRegion} set_region={setRegion} />
-            <TabItem title="South America" region="South America" selected_region={selectedRegion} set_region={setRegion} />
-            <TabItem title="Asia" region="Asia" selected_region={selectedRegion} set_region={setRegion} />
-            <TabItem title="Middle East" region="Middle East" selected_region={selectedRegion} set_region={setRegion} />
-            <TabItem title="Africa" region="Africa" selected_region={selectedRegion} set_region={setRegion} />
-        </div>
-    )
-}
+  const [index, setIndex] = useState(2);
+  return (
+    <div className="grid grid-cols-9 border-b-[1px] border-[#F2B21B70] font-montserrat font-semibold max-md:hidden">
+      <TabItem
+        title="Africa"
+        region="Africa"
+        selected_region={selectedRegion}
+        set_region={setRegion}
+      />
+      <TabItem
+        title="Asia"
+        region="Asia"
+        selected_region={selectedRegion}
+        set_region={setRegion}
+      />
+      <TabItem
+        title="Caribbean"
+        region="Caribbean"
+        selected_region={selectedRegion}
+        set_region={setRegion}
+      />
+      <TabItem
+        title="Europe"
+        region="Europe"
+        selected_region={selectedRegion}
+        set_region={setRegion}
+      />
+      <TabItem
+        title="LATAM"
+        region="LATAM"
+        selected_region={selectedRegion}
+        set_region={setRegion}
+      />
+      <TabItem
+        title="Middle East"
+        region="Middle East"
+        selected_region={selectedRegion}
+        set_region={setRegion}
+      />
+
+      <TabItem
+        title="North America"
+        region="North America"
+        selected_region={selectedRegion}
+        set_region={setRegion}
+      />
+      <TabItem
+        title="Oceania"
+        region="Oceania"
+        selected_region={selectedRegion}
+        set_region={setRegion}
+      />
+      <TabItem
+        title="Global"
+        region="Global"
+        selected_region={selectedRegion}
+        set_region={setRegion}
+      />
+    </div>
+  );
+};
