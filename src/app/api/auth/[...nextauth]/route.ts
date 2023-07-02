cf4a5f5d5f54b0ea3f852cb2ee21eb3d3585b6e1
21st-Athena <132208831+21st-Athena@users.noreply.github.com>
2023-07-02 17:22:27 -0400

    add/google-auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -12,7 +12,7 @@ const authOptions: any = {
                 }),
             ],
             pages: {
-                error: "/sss",
+                error: "/",
                 signIn: '/auth/signin',
                 signOut: '/auth/signout',
             },
@@ -32,36 +32,3 @@ const authOptions: any = {
         };
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
-
-// export default async function handler(
-//     req: NextApiRequest,
-//     res: NextApiResponse
-// ) {
-//     const authOptions: any = {
-//         providers: [
-//             GoogleProvider({
-//                 clientId: "185531830886-iqnpvpqfoqftj4mg8hv5c5k6odd6tb4h.apps.googleusercontent.com",
-//                 clientSecret:"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",
-//             }),
-//         ],
-//         pages: {
-//             error: "/sss",
-//             signIn: '/auth/signin',
-//             signOut: '/auth/signout',
-//         },
-//         callbacks: {
-//             // async session({ session }: any) {
-//             //         return Promise.resolve(session);
-//             // },
-//             async signIn({ user }: any) {
-//                 console.log(user);
-//                 try {
-//                     return Promise.resolve(true);
-//                 } catch (err) {
-//                     return Promise.reject(new Error(SERVER_ERR_MSG));
-//                 }
-//             },
-//         },
-//     };
-//     return await NextAuth(req, res, authOptions);
-// }
