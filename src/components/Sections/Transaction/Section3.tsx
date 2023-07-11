ad7c4a54cd95281c87da629b6bfd71ce44868085
21st-Athena <132208831+21st-Athena@users.noreply.github.com>
2023-07-11 03:56:11 -0400

    add/helpers

--- src/components/Sections/Transaction/Section3.tsx
+++ src/components/Sections/Transaction/Section3.tsx
@@ -51,82 +51,85 @@ export const Section3 = () => {
 
   return (
     <section className="mi-medium:px-[300px] 2xl:px-[100px] px-6 py-[40px] bg-white text-dark-solid text-center flex flex-col gap-10">
-      {is_Loading ? <Spinner /> : ""}
-      <div className="grid lg:grid-cols-2 grid-cols-1 gap-[30px]">
-        <div className="flex flex-col gap-[60px]">
-          <div className="flex flex-row gap-4">
-            <Image
-              src="/assets/Transaction Page/Icons/Check mark.svg"
-              width={64}
-              height={64}
-              alt="Check Mark"
-            />
-            <div className="lg:text-[24px] text-[20px] grid grid-rows-2 font-montserratbold text-left">
-              <p>Order #{transactionData?.order_number}</p>
-              <p>Thanks Helex Mofidex</p>
+      {is_Loading ? (
+        <Spinner />
+      ) : (
+        <div className="grid lg:grid-cols-2 grid-cols-1 gap-[30px]">
+          <div className="flex flex-col gap-[60px]">
+            <div className="flex flex-row gap-4">
+              <Image
+                src="/assets/Transaction Page/Icons/Check mark.svg"
+                width={64}
+                height={64}
+                alt="Check Mark"
+              />
+              <div className="lg:text-[24px] text-[20px] grid grid-rows-2 font-montserratbold text-left">
+                <p>Order #{transactionData?.order_number}</p>
+                <p>Thanks Helex Mofidex</p>
+              </div>
             </div>
-          </div>
-          <hr className="border-[#DDDDDD]" />
-          <div className="flex flex-row gap-8 max-lg:mx-auto">
-            <Button
-              title="QR code"
-              color="#FFECBE"
-              selectedColor="#F2B21B"
-              selected={!selected_type}
-              onClick={() => handleClickType("qr_code")}
-            />
-            <Button
-              title="Manual"
-              color="#FFECBE"
-              selectedColor="#F2B21B"
-              selected={selected_type}
-              onClick={() => handleClickType("manual")}
-            />
-          </div>
-          {selected_type ? (
-            <p className="text-[25px]">{transactionData?.qr_code}</p>
-          ) : (
-            <QRCode
-              size={256}
-              style={{
-                height: "auto",
-                maxWidth: "300px",
-                width: "300px",
-                margin: "auto",
-              }}
-              value={transactionData?.qr_code ? transactionData?.qr_code : ""}
-              viewBox={`0 0 256 256`}
-            />
-          )}
+            <hr className="border-[#DDDDDD]" />
+            <div className="flex flex-row gap-8 max-lg:mx-auto">
+              <Button
+                title="QR code"
+                color="#FFECBE"
+                selectedColor="#F2B21B"
+                selected={!selected_type}
+                onClick={() => handleClickType("qr_code")}
+              />
+              <Button
+                title="Manual"
+                color="#FFECBE"
+                selectedColor="#F2B21B"
+                selected={selected_type}
+                onClick={() => handleClickType("manual")}
+              />
+            </div>
+            {selected_type ? (
+              <p className="text-[25px]">{transactionData?.qr_code}</p>
+            ) : (
+              <QRCode
+                size={256}
+                style={{
+                  height: "auto",
+                  maxWidth: "300px",
+                  width: "300px",
+                  margin: "auto",
+                }}
+                value={transactionData?.qr_code ? transactionData?.qr_code : ""}
+                viewBox={`0 0 256 256`}
+              />
+            )}
 
-          {/* Integrate With API */}
-          <Roaming
-            iccid={transactionData?.iccid}
-            country={transactionData?.country_name}
-          />
-          <Reload />
-          <Info
-            full_name={transactionData?.request.full_name}
-            email={transactionData?.request.email}
-            phone_number={transactionData?.request.phone_number}
-          />
-          <ContactUs />
-          <hr className="border-[#DDDDDD] max-lg:hidden" />
-          <p className="font-montserrat text-[18px] text-left max-lg:hidden">
-            Purchase options cancellation policy
-          </p>
+            {/* Integrate With API */}
+            <Roaming
+              iccid={transactionData?.iccid}
+              country={transactionData?.country_name}
+            />
+            <Reload />
+            <Info
+              full_name={transactionData?.request.full_name}
+              email={transactionData?.request.email}
+              phone_number={transactionData?.request.phone_number}
+            />
+            <ContactUs />
+            <hr className="border-[#DDDDDD] max-lg:hidden" />
+            <p className="font-montserrat text-[18px] text-left max-lg:hidden">
+              Purchase options cancellation policy
+            </p>
+          </div>
+          <div className="max-lg:order-first">
+            <Card
+              country={selected_package?.country_code}
+              title={selected_package?.country_name}
+              size={selected_package?.data_GB}
+              price={Number(selected_package?.price)}
+              subtotal={Number(selected_package?.price)}
+              discount={1.0}
+            />
+          </div>
         </div>
-        <div className="max-lg:order-first">
-          <Card
-            country={selected_package?.country_code}
-            title={selected_package?.country_name}
-            size={selected_package?.data_GB}
-            price={Number(selected_package?.price)}
-            subtotal={Number(selected_package?.price)}
-            discount={1.0}
-          />
-        </div>
-      </div>
+      )}
     </section>
   );
 };
