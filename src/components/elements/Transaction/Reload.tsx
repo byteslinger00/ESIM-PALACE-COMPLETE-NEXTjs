20b1dff78d831abebf6f681dca8dbb82dd34866c
21st-Athena <132208831+21st-Athena@users.noreply.github.com>
2023-07-13 22:44:06 -0400

    fix/deploy-stripe

--- src/components/elements/Transaction/Reload.tsx
+++ src/components/elements/Transaction/Reload.tsx
@@ -9,12 +9,12 @@ const stripePromise = loadStripe(
 );
 export const Reload = () => {
   const router = useRouter();
-
   const onCheckout = async () => {
     const response = await checkout();
+    console.log(response);
     router.push(response || '');
   }
-  
+
   return (
     <div className="flex flex-col gap-6">
       <p className="font-montserratbold lg:text-[24px] text-[16px] text-left">
@@ -25,40 +25,6 @@ export const Reload = () => {
           Checkout
         </p>
       </button>
-      {/* <form action="/api/checkout_sessions" method="POST">
-      <section>
-        <button type="submit" role="link">
-          Checkout
-        </button>
-      </section>
-      <style jsx>
-        {`
-          section {
-            background: #ffffff;
-            display: flex;
-            flex-direction: column;
-            width: 400px;
-            height: 112px;
-            border-radius: 6px;
-            justify-content: space-between;
-          }
-          button {
-            height: 36px;
-            background: #556cd6;
-            border-radius: 4px;
-            color: white;
-            border: 0;
-            font-weight: 600;
-            cursor: pointer;
-            transition: all 0.2s ease;
-            box-shadow: 0px 4px 5.5px 0px rgba(0, 0, 0, 0.07);
-          }
-          button:hover {
-            opacity: 0.8;
-          }
-        `}
-      </style>
-    </form> */}
     </div>
   );
 };
