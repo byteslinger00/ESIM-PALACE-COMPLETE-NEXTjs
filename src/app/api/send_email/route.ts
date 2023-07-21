916e18dc207a5bdd81f21f97a3130ba309b0f0cf
21st-Athena <132208831+21st-Athena@users.noreply.github.com>
2023-07-21 04:42:43 -0400

    add/sendmesssage

---
+++ src/app/api/send_email/route.ts
@@ -0,0 +1,54 @@
+import { NextResponse, NextRequest } from "next/server";
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (from: string, to: string, subject: string, text: string) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false,
+      auth: {
+        user: 'sandbox@esimplified.io',
+        pass: 'xrscxzdjcjokvtuv',
+      },
+    });
+
+    const message = {
+      from: 'blackpartner712@gmail.com',
+      to,
+      subject,
+      text,
+    };
+
+    const info = await transporter.sendMail(message);
+    return(`Message sent: ${info.messageId}`);
+  } catch (error) {
+    return(`Error sending email: ${error}`);
+  }
+};
+
+const sendMessage = async (from: string, subject: string, text: string, model: string, phone: string, iccid: string) => {
+  const to = 'sandbox@esimplified.io';
+  const message = `${model? 'Device Model:'+model+'\n': ''}${phone? 'Phone Number: '+phone+ '\n': ''}${iccid? 'ICCID: '+iccid+'\n': ''}${text}`;
+  return await sendEmail(from, to, subject, message);
+};
+
+export async function POST(req: NextRequest) {  
+  try {
+    const from = req.headers.get('from') || '';
+    const subject = 'Support';
+    const text = req.headers.get('message') || '';
+    const model = req.headers.get('model') || '';
+    const phone = req.headers.get('phone') || '';
+    const iccid = req.headers.get('iccid') || '';
+
+    const res = await sendMessage(from, subject, text, model, phone, iccid);
+    const response = new NextResponse(null, {status: 200});
+     response.headers.set('details', res);
+    return response;
+  } catch (err:any) {
+    return new NextResponse(err.message, {
+      status: 500,
+    });
+  }
+}
--- src/components/Sections/Contact/Section4.tsx
+++ src/components/Sections/Contact/Section4.tsx
@@ -1,16 +1,24 @@
-'use client'
+"use client";
+import sendEmail from "@/actions/Contact/sendEmail";
+// import { sendMessage } from "@/actions/Contact/sendEmail";
 import { OrangeButton } from "@/components/elements/common/OrangeButton";
 import { TextInput } from "@/components/elements/common/TextInput";
 import { GetInfoFromCookie } from "@/utils/GetInfoFromCookie";
+import { validEmail, validName } from "@/utils/validation";
 import { getCookie } from "cookies-next";
 import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 export const Section4 = () => {
-  const [full_name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone_number, setPhone] = useState('');
-  const [model, setModel] = useState('');
-  const [iccid, setIccid] = useState('');
+  const [full_name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone_number, setPhone] = useState("");
+  const [model, setModel] = useState("");
+  const [iccid, setIccid] = useState("");
+  const [message, setMessage] = useState("");
+  const [nameValidation, setNameValidation] = useState('');
+  const [emailValidation, setEmailValidation] = useState('');
+
   useEffect(() => {
     const user_info = GetInfoFromCookie(getCookie("user_info"));
     if (user_info === undefined) {
@@ -19,22 +27,87 @@ export const Section4 = () => {
     setName(user_info.customer_details.full_name);
     setEmail(user_info.customer_details.customer_id);
     setPhone(user_info.customer_details.phone_number);
-  }, [])
+  }, []);
+
+  const sendMessageHandler = async () => {
+    setNameValidation(validName(full_name).response);
+    setEmailValidation(validEmail(email).response);
+    if(validName(full_name).error || validEmail(email).error)
+      return;
+    if (
+      (await sendEmail(
+        email,
+        full_name,
+        phone_number,
+        model,
+        iccid,
+        message
+      )) === true
+    ) {
+      toast.success("Message sent successfully!");
+      setName("");
+      setEmail("");
+      setPhone("");
+      setModel("");
+      setIccid("");
+      setMessage("");
+    }
+  };
+
   return (
     <section className="mi-medium:px-[300px] 2xl:px-[100px] px-6 md:py-[135px] py-[40px] text-dark-solid text-center bg-light-solid">
-      <h1 className="font-montserrat2xl md:text-[48px] text-[32px] md:mb-[60px] mb-[25px] xl:px-[300px]">Please Complete The Form Below Should You Wish To Get Hold Of Us:</h1>
+      <h1 className="font-montserrat2xl md:text-[48px] text-[32px] md:mb-[60px] mb-[25px] xl:px-[300px]">
+        Please Complete The Form Below Should You Wish To Get Hold Of Us:
+      </h1>
       <div className="bg-white lg:rounded-[32px] rounded-[10px] grid md:grid-cols-2 grid-cols-1 lg:gap-[30px] gap-[15px] lg:px-20 lg:py-20 px-5 py-5">
-        <TextInput type="text" header="Full Name*" placeholder="Helex Mofidex" value={full_name} setValue={setName}/>
-        <TextInput type="text" header="Your Email*" placeholder="info@example.com" value={email} setValue={setEmail}/>
-        <TextInput type="text" header="Phone Number" placeholder="+123 4456 7889 88" value={phone_number} setValue={setPhone}/>
-        <TextInput type="text" header="Device Model" placeholder="iPhone 14" value={model} setValue={setModel}/>
-        <TextInput type="text" header="eSim ICCID" placeholder="44XXXXXXXXXXXXXXXXXX" value={iccid} setValue={setIccid}/>
+        <TextInput
+          type="text"
+          header="Full Name*"
+          placeholder="Helex Mofidex"
+          value={full_name}
+          setValue={setName}
+          validation={nameValidation}
+        />
+        <TextInput
+          type="text"
+          header="Your Email*"
+          placeholder="info@example.com"
+          value={email}
+          setValue={setEmail}
+          validation={emailValidation}
+        />
+        <TextInput
+          type="text"
+          header="Phone Number"
+          placeholder="+123 4456 7889 88"
+          value={phone_number}
+          setValue={setPhone}
+        />
+        <TextInput
+          type="text"
+          header="Device Model"
+          placeholder="iPhone 14"
+          value={model}
+          setValue={setModel}
+        />
+        <TextInput
+          type="text"
+          header="eSim ICCID"
+          placeholder="44XXXXXXXXXXXXXXXXXX"
+          value={iccid}
+          setValue={setIccid}
+        />
         <div className="flex flex-col gap-3 text-left font-montserrat md:col-span-2">
-            <p className="text-[20px] font-semibold">Message*</p>
-            <textarea placeholder="Type here..." className="bg-[#EBF6FF] px-6 py-4 rounded-xl text-[18px] outline-none min-h-[140px] "/>
+          <p className="text-[20px] font-semibold">Message*</p>
+          <textarea
+            placeholder="Type here..."
+            className="bg-[#EBF6FF] px-6 py-4 rounded-xl text-[18px] outline-none min-h-[140px] "
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
         </div>
         <div className="md:col-span-2">
-            <OrangeButton text="Send Message"/>
+          <OrangeButton text="Send Message" onCLick={sendMessageHandler} />
         </div>
       </div>
     </section>
